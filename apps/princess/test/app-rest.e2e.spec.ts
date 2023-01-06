9db8f98c13209e69354308f020d05f686a8149c1
polartar <polaris.center@outlook.com>
2023-01-07 00:28:52 +0100

    feat(princess): Add REST support

---
+++ apps/princess/test/app-rest.e2e.spec.ts
@@ -0,0 +1,28 @@
+import { INestApplication } from '@nestjs/common'
+import { Test, TestingModule } from '@nestjs/testing'
+import { AppController } from '../src/app/app.controller'
+import { AppService } from '../src/app/app.service'
+
+describe('AnonGateway', () => {
+  let app: INestApplication
+
+  beforeAll(async () => {
+    // Initialize and start the server
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile()
+
+    app = module.createNestApplication()
+    await app.listen(3333)
+  })
+
+  describe('Welcome Message', () => {
+    it('', async () => {
+      expect.assertions(2)
+      const data = await fetch('http://localhost:3333/')
+      expect(data.status).toEqual(200)
+      expect(await data.text()).toEqual(AppService.welcomeMessage)
+    })
+  })
+})
