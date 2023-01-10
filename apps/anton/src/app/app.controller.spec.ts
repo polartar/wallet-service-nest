46634636bf0ac0fd94e05642cc5cbb45f868e25b
polartar <polaris.center@outlook.com>
2023-01-10 17:44:57 +0100

    feat(anton): Sign and Verify skeletons

--- apps/anton/src/app/app.controller.spec.ts
+++ apps/anton/src/app/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
   describe('getData', () => {
     it('should return "Welcome to anton!"', () => {
       const appController = app.get<AppController>(AppController)
-      expect(appController.welcome).toEqual(AppService.welcomeMessage)
+      expect(appController.welcome()).toEqual(AppService.welcomeMessage)
     })
   })
 })
