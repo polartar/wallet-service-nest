9c3a95c728c8bbac89f9abf5ad123056053e4675
polartar <polaris.center@outlook.com>
2023-01-20 06:55:53 -0800

    fix(gandalf): fix lint errors

--- apps/gandalf/src/auth/auth.controller.ts
+++ apps/gandalf/src/auth/auth.controller.ts
@@ -1,25 +1,24 @@
-import { AuthService } from './auth.service';
+import { AuthService } from './auth.service'
 import { BadRequestException, Body, Controller, Post } from '@nestjs/common'
 
 export type IAuthData = {
-  idToken: string,
+  idToken: string
   type: 'GOOGLE' | 'APPLE'
 }
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly service: AuthService) {}
+  constructor(private readonly service: AuthService) {}
 
-    @Post()
-    async login(@Body() data: IAuthData) {
-        if (!data.idToken) {
-            throw new BadRequestException("Token can't be null");
-        }
-        try {
-            const response = await this.service.authorize(data)
-            return response;
-        } catch(err) {
-            throw new BadRequestException(err.message);
-        }
-
+  @Post()
+  async login(@Body() data: IAuthData) {
+    if (!data.idToken) {
+      throw new BadRequestException("Token can't be null")
     }
+    try {
+      const response = await this.service.authorize(data)
+      return response
+    } catch (err) {
+      throw new BadRequestException(err.message)
+    }
+  }
 }
--- apps/gandalf/src/auth/auth.service.ts
+++ apps/gandalf/src/auth/auth.service.ts
@@ -1,41 +1,43 @@
 import { Injectable } from '@nestjs/common'
 import { IAuthData } from './auth.controller'
-import { OAuth2Client } from 'google-auth-library';
-import verifyAppleToken from "verify-apple-id-token";
+import { OAuth2Client } from 'google-auth-library'
+import verifyAppleToken from 'verify-apple-id-token'
 
 @Injectable()
 export class AuthService {
-    async authorize(data: IAuthData): Promise<string> {
-        if (data.type === 'GOOGLE') {
-            const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
-            try {
-                const response = await client.verifyIdToken({
-                    idToken: data.idToken,
-                    audience: process.env.GOOGLE_CLIENT_ID});
-                const payload = response.getPayload();
-                if (payload.iss !== 'accounts.google.com' && payload.aud !== process.env.GOOGLE_CLIENT_ID)
-                    throw new Error("Invalid token");
-                const email = payload.email;
+  async authorize(data: IAuthData): Promise<string> {
+    if (data.type === 'GOOGLE') {
+      const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID)
+      try {
+        const response = await client.verifyIdToken({
+          idToken: data.idToken,
+          audience: process.env.GOOGLE_CLIENT_ID,
+        })
+        const payload = response.getPayload()
+        if (
+          payload.iss !== 'accounts.google.com' &&
+          payload.aud !== process.env.GOOGLE_CLIENT_ID
+        )
+          throw new Error('Invalid token')
+        const email = payload.email
 
-                return email;
-            } catch(err) {
-                throw new Error("Invalid token")
-            }
-        } else if (data.type === 'APPLE') {
-            try {
-                const jwtClaims = await verifyAppleToken({
-                    idToken: data.idToken,
-                    clientId: process.env.APPLE_CLIENT_ID,
-                });
+        return email
+      } catch (err) {
+        throw new Error('Invalid token')
+      }
+    } else if (data.type === 'APPLE') {
+      try {
+        const jwtClaims = await verifyAppleToken({
+          idToken: data.idToken,
+          clientId: process.env.APPLE_CLIENT_ID,
+        })
 
-                return jwtClaims.email; 
-            } catch(err) {
-                throw new Error("Invalid token")
-            }
-            
-
-        } else {
-            throw new Error("Unsupported type")
-        }
+        return jwtClaims.email
+      } catch (err) {
+        throw new Error('Invalid token')
+      }
+    } else {
+      throw new Error('Unsupported type')
     }
+  }
 }
