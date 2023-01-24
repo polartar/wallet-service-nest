d69f24c09dee829efde6414aca0d55b5cdf10827
polartar <polaris.center@outlook.com>
2023-01-24 12:10:17 +0100

    feat(gandalf): Use validation pipe for Joi

--- apps/gandalf/src/auth/auth.controller.ts
+++ apps/gandalf/src/auth/auth.controller.ts
@@ -1,31 +1,15 @@
 import { AuthService } from './auth.service'
-import { BadRequestException, Body, Controller, Post } from '@nestjs/common'
-// import {Joi} from 'joi'
-import Joi = require('joi')
-import { IAuthData, EAuth } from './auth.types'
+import { Body, Controller, Post, UsePipes } from '@nestjs/common'
+import { IAuthData } from './auth.types'
+import { LoginValidationPipe } from './auth.pipe'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly service: AuthService) {}
 
   @Post()
+  @UsePipes(new LoginValidationPipe())
   async login(@Body() data: IAuthData) {
-    const schema = Joi.object().keys({
-      idToken: Joi.string().regex(
-        /^[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_.+/=]*$/,
-      ),
-      type: Joi.string().valid(EAuth.Google, EAuth.Apple),
-    })
-
-    try {
-      const res = schema.validate(data)
-      if (res.error) {
-        throw new Error(res.error.message)
-      }
-
-      return await this.service.authorize(data)
-    } catch (err) {
-      throw new BadRequestException(err.message)
-    }
+    return this.service.authorize(data)
   }
 }
