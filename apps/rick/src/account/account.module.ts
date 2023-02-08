9d4cb1661d0a690735369913f93ec9fa46b355ba
polartar <polaris.center@outlook.com>
2023-02-08 07:25:29 -0800

    fix[rick]: use real database

--- apps/rick/src/account/account.module.ts
+++ apps/rick/src/account/account.module.ts
@@ -7,15 +7,21 @@ import { AccountService } from './account.service'
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'better-sqlite3',
-      database: ':memory:',
-      dropSchema: true,
-      synchronize: true,
+      type: 'postgres',
+      host: 'localhost',
+      port: 5431,
+      username: 'myusername',
+      password: 'mypassword',
+      database: 'rick',
       entities: [
         AccountEntity, //
         WalletEntity,
       ],
+      // TODO: Maybe disable in production?
+      autoLoadEntities: true,
+      synchronize: true,
     }),
+
     TypeOrmModule.forFeature([
       AccountEntity, //
       WalletEntity,
