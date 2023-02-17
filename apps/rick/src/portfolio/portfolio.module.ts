af950e45adb89033b8b256774bc6d0107a1a2265
polartar <polaris.center@outlook.com>
2023-02-17 10:18:02 -0800

    fix(rick): lint issue

--- apps/rick/src/portfolio/portfolio.module.ts
+++ apps/rick/src/portfolio/portfolio.module.ts
@@ -1,6 +1,6 @@
 import { WalletEntity } from './../wallet/wallet.entity'
 import { WalletModule } from './../wallet/wallet.module'
-import { Inject, Module, forwardRef } from '@nestjs/common'
+import { Module, forwardRef } from '@nestjs/common'
 import { PortfolioService } from './portfolio.service'
 import { AccountModule } from '../account/account.module'
 import { ConfigModule } from '@nestjs/config'
@@ -8,7 +8,6 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { Environment } from '../environments/environment.dev'
 import { AccountEntity } from '../account/account.entity'
 import { HttpModule } from '@nestjs/axios'
-import { WalletService } from '../wallet/wallet.service'
 
 @Module({
   imports: [
