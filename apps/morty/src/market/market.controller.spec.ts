e62df2020276c92ffd6e2f9dac04ab24010b986d
polartar <polaris.center@outlook.com>
2023-03-07 07:58:23 -0800

    fix(morty): add missing config module in the test

--- apps/morty/src/market/market.controller.spec.ts
+++ apps/morty/src/market/market.controller.spec.ts
@@ -1,12 +1,21 @@
+import { HttpModule } from '@nestjs/axios'
 import { Test, TestingModule } from '@nestjs/testing'
 import { MarketController } from './market.controller'
+import { MarketService } from './market.service'
+import { ConfigModule } from '@nestjs/config'
+import { Environment } from '../environments/environment.dev'
 
 describe('MarketController', () => {
   let controller: MarketController
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        HttpModule, //
+        ConfigModule.forRoot({ load: [Environment] }),
+      ],
       controllers: [MarketController],
+      providers: [MarketService],
     }).compile()
 
     controller = module.get<MarketController>(MarketController)
--- apps/morty/src/market/market.service.spec.ts
+++ apps/morty/src/market/market.service.spec.ts
@@ -1,11 +1,18 @@
+import { HttpModule } from '@nestjs/axios'
 import { Test, TestingModule } from '@nestjs/testing'
 import { MarketService } from './market.service'
+import { ConfigModule } from '@nestjs/config'
+import { Environment } from '../environments/environment.dev'
 
 describe('MarketService', () => {
   let service: MarketService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        HttpModule, //
+        ConfigModule.forRoot({ load: [Environment] }),
+      ],
       providers: [MarketService],
     }).compile()
 
