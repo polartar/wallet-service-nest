b4306990cba07ede3d65c794e7459ae9073b5fd4
polartar <polaris.center@outlook.com>
2023-03-08 07:35:32 -0800

    chore(morty): use defined types for the environment keys

--- apps/morty/src/environments/environment.dev.ts
+++ apps/morty/src/environments/environment.dev.ts
@@ -3,10 +3,10 @@ import { IData } from '@rana/core'
 import { EEnvironment } from './environment.types'
 export const Environment = () => {
   const env: { [key in EEnvironment]: IData } = {
-    coin_market_api: process.env.COINMARKET_API_KEY,
-    is_production: process.env.NODE_ENV === 'production',
-    fidelity_client_id: process.env.FIDELITY_CLIENT_ID,
-    fidelity_client_secret: process.env.FIDELITY_CLIENT_SECRET,
+    [EEnvironment.coinMarketAPI]: process.env.COINMARKET_API_KEY,
+    [EEnvironment.isProduction]: process.env.NODE_ENV === 'production',
+    [EEnvironment.fidelityClientId]: process.env.FIDELITY_CLIENT_ID,
+    [EEnvironment.fidelityClientSecret]: process.env.FIDELITY_CLIENT_SECRET,
   }
   return env
 }
