b5acb75e256afc7bfae69bee11fca6979bad20e1
polartar <polaris.center@outlook.com>
2023-03-12 07:47:08 -0700

    feat(princess): implement unit test for news service

--- apps/princess/src/news/news.service.spec.ts
+++ apps/princess/src/news/news.service.spec.ts
@@ -1,11 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { NewsService } from './news.service'
+import { ConfigModule } from '@nestjs/config'
+import { Environment } from './../environments/environment.dev'
+import { HttpModule } from '@nestjs/axios'
 
 describe('NewsService', () => {
   let service: NewsService
 
   beforeEach(async () => {
+    jest.setTimeout(60000)
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({ load: [Environment] }), //
+        HttpModule,
+      ],
       providers: [NewsService],
     }).compile()
 
@@ -14,5 +22,23 @@ describe('NewsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  it('should get top 5 news', async () => {
+    const news = await service.getTopNews(5)
+    expect((news as { data: [] }).data.length).toBe(5)
+  })
+
+  it('should get 5 news with pagination', async () => {
+    const response = (await service.getNews({
+      pageNumber: 1,
+      countPerPage: 5,
+    })) as {
+      success: boolean
+      data: { news: []; countPerPage: number; currentPage: number }
+    }
+
+    expect(response.data.news.length).toBe(5)
+    expect(response.data.currentPage).toBe(1)
   })
 })
