123eca45ef770256e5eb4d5dfa01eff0e419106f
polartar <polaris.center@outlook.com>
2023-04-21 08:18:20 -0700

    feat(princess): add swagger response for news endpoints

--- apps/princess/src/news/dto/NewsTopDto.ts
+++ apps/princess/src/news/dto/NewsTopDto.ts
@@ -15,3 +15,26 @@ export class NewsTopDto {
   @IsOptional()
   symbol: ECoinType
 }
+
+export class TopNewsResponse {
+  @ApiProperty({ example: true })
+  success: boolean
+
+  @ApiProperty({
+    example: [
+      {
+        pubDateUtc: '2023-04-21T14:04:55.000Z',
+        title:
+          'OP Erigon released as new client software for Optimism on testnet',
+        author: 'Vishal Chawla',
+        source: 'theblock',
+        description:
+          "Optimism contributors emphasize that multiple clients can help prevent a single point of failure, bolstering the network's resilience.",
+        link: 'https://www.theblock.co/post/227593/optimism-client-op-erigon-released?utm_source=fidelity&utm_medium=rss',
+        textCleaned: 'Test in',
+        assetList: ['ETH'],
+      },
+    ],
+  })
+  data: []
+}
--- apps/princess/src/news/news.controller.ts
+++ apps/princess/src/news/news.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get, Query, UsePipes } from '@nestjs/common'
 import { NewsService } from './news.service'
 import { NewsValidationPipe } from './news.pipe'
-import { ApiOperation, ApiTags } from '@nestjs/swagger'
-import { NewsPaginationDto } from './dto/NewsPagination.dto'
-import { NewsTopDto } from './dto/NewsTopDto'
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger'
+import {
+  NewsPaginationDto,
+  PaginationNewsResponse,
+} from './dto/NewsPagination.dto'
+import { NewsTopDto, TopNewsResponse } from './dto/NewsTopDto'
 
 @Controller('news')
 @ApiTags('news')
@@ -11,6 +14,7 @@ export class NewsController {
   constructor(private readonly newsService: NewsService) {}
 
   @Get('top')
+  @ApiOkResponse({ type: TopNewsResponse })
   @ApiOperation({ summary: 'Get the top news' })
   getLatestNews(@Query() query?: NewsTopDto) {
     return this.newsService.getLatestNews(query.count, query.symbol)
@@ -18,6 +22,7 @@ export class NewsController {
 
   @Get()
   @ApiOperation({ summary: 'Get the news with pagination' })
+  @ApiOkResponse({ type: PaginationNewsResponse })
   @UsePipes(new NewsValidationPipe())
   getNews(
     @Query()
