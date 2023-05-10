5ca53339edb6e85aaa228caf4777491d42f64b6c
polartar <polaris.center@outlook.com>
2023-05-10 08:37:41 -0700

    chore(bristle): remove unused env variable

--- apps/bristle/src/environments/environment.dev.ts
+++ apps/bristle/src/environments/environment.dev.ts
@@ -4,7 +4,6 @@ import { EEnvironment } from './environment.types'
 export const Environment = () => {
   const env: { [key in EEnvironment]: IData } = {
     [EEnvironment.isProduction]: process.env.NODE_ENV === 'production',
-    [EEnvironment.liquidAPIKey]: process.env.LIQUID_API_KEY,
     [EEnvironment.princessAPIUrl]:
       process.env.PRINCESS_API_URL || 'http://localhost3000',
   }
--- apps/bristle/src/environments/environment.types.ts
+++ apps/bristle/src/environments/environment.types.ts
@@ -1,5 +1,4 @@
 export enum EEnvironment {
   isProduction = 'is_production',
-  liquidAPIKey = 'liquid_api_key',
   princessAPIUrl = 'princess_api_url',
 }
