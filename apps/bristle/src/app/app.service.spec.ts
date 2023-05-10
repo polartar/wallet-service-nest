01197464b4070ccb36535e313585967353e1bb51
polartar <polaris.center@outlook.com>
2023-05-10 05:15:08 -0700

    feat(bristle): implement unit test for bc-ur

--- apps/bristle/src/app/app.service.spec.ts
+++ apps/bristle/src/app/app.service.spec.ts
@@ -1,6 +1,7 @@
 import { Test } from '@nestjs/testing'
 
 import { AppService } from './app.service'
+import { UR, UREncoder } from '@ngraveio/bc-ur'
 
 describe('AppService', () => {
   let service: AppService
@@ -16,6 +17,21 @@ describe('AppService', () => {
   describe('getData', () => {
     it('should return "Welcome to bristle!"', () => {
       expect(service.welcomeMessage).toEqual(AppService.welcomeMessage)
+    })
+
+    it('should verify the payload', () => {
+      const message = { message1: 'property1' }
+      const messageBuffer = Buffer.from(JSON.stringify(message))
+
+      const ur = UR.fromBuffer(messageBuffer)
+      const maxFragmentLength = 150
+      const firstSeqNum = 0
+
+      const encoder = new UREncoder(ur, maxFragmentLength, firstSeqNum)
+
+      const part = encoder.nextPart()
+
+      expect(service.verifyPayload([part])).toBe(JSON.stringify(message))
     })
   })
 })
