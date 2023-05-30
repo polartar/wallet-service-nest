174718642e9f4f905f68933cfb964a3c4fa7429d
polartar <polaris.center@outlook.com>
2023-05-30 12:58:30 -0700

    chore(princess): swagger response

--- apps/princess/src/accounts/dto/GetPortfolioDto.ts
+++ apps/princess/src/accounts/dto/GetPortfolioDto.ts
@@ -59,11 +59,9 @@ export class PortfolioSwaggerResponse {
         },
         history: [
           {
-            id: 1,
             balance: '126946376672449164',
             from: '0xdBC3A556693CBb5682127864fd80C8ae6976bfcf',
             to: '0xdBC3A556693CBb5682127864fd80C8ae6976bfcf',
-            hash: '0xa70b380def09016af59abeb878d7a38f09d1a7026b1d6281cce007e9cfac8c29',
             amount: '0',
             tokenId: 52852,
             timestamp: '1685463120',
@@ -71,11 +69,9 @@ export class PortfolioSwaggerResponse {
             usdAmount: '0',
           },
           {
-            id: 2,
             balance: '227433406994413955',
             from: '0xe456f9A32E5f11035ffBEa0e97D1aAFDA6e60F03',
             to: '0xdBC3A556693CBb5682127864fd80C8ae6976bfcf',
-            hash: '0xec7c67d42f2dfec448f26f1b30d42fb9ebe07f6975f6b846a19a43484ad35781',
             amount: '10000000000000000',
             tokenId: null,
             timestamp: '1682560092',
