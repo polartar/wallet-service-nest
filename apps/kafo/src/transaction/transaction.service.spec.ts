05c0dd58e4d804f27c408610830ef34256ac4de6
polartar <polaris.center@outlook.com>
2023-06-05 07:19:34 -0700

    add console log

--- apps/kafo/src/transaction/transaction.service.spec.ts
+++ apps/kafo/src/transaction/transaction.service.spec.ts
@@ -35,6 +35,7 @@ describe('TransactionService', () => {
     }
 
     const response = await service.generate(transactionData)
+    console.log({ response })
     expect(response.success).toBeTruthy()
   }, 30000)
 })
