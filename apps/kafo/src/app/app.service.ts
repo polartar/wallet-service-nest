42810607ef101b742c2dfe10ee4cd08f7532a211
polartar <polaris.center@outlook.com>
2023-06-12 07:37:34 -0700

    feat(kafo): check env vars

--- apps/kafo/src/app/app.service.ts
+++ apps/kafo/src/app/app.service.ts
@@ -1,10 +1,31 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, InternalServerErrorException } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { EEnvironment } from '../environments/environment.types'
 
 @Injectable()
 export class AppService {
   static welcomeMessage = 'Kafo is up and running'
 
+  constructor(private configService: ConfigService) {
+    this.checkEnvs()
+    // this.isProduction = this.configService.get<boolean>(
+    //   EEnvironment.isProduction,
+    // )
+  }
   get welcome() {
     return AppService.welcomeMessage
+  }
+
+  checkEnvs() {
+    Object.keys(EEnvironment).map((envKey) => {
+      const value = this.configService.get<string | boolean>(
+        EEnvironment[envKey],
+      )
+      if (!value && value !== false) {
+        throw new InternalServerErrorException(
+          `Env variable(${envKey}) is missing`,
+        )
+      }
+    })
   }
 }
--- apps/kafo/src/transaction/transaction.module.ts
+++ apps/kafo/src/transaction/transaction.module.ts
@@ -2,14 +2,9 @@ import { Module } from '@nestjs/common'
 import { TransactionController } from './transaction.controller'
 import { TransactionService } from './transaction.service'
 import { HttpModule } from '@nestjs/axios'
-import { ConfigModule } from '@nestjs/config'
-import { Environment } from './../environments/environment.dev'
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ load: [Environment] }), //
-    HttpModule,
-  ],
+  imports: [HttpModule],
   controllers: [TransactionController],
   providers: [TransactionService],
 })
