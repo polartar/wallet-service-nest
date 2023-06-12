12f189049e5398378464e7dad342a97837beb9d3
polartar <polaris.center@outlook.com>
2023-06-12 09:52:32 -0700

    fix: wrong node_env value

--- apps/bristle/src/environments/environment.dev.ts
+++ apps/bristle/src/environments/environment.dev.ts
@@ -3,7 +3,7 @@ import { IData } from '@rana/core'
 import { EEnvironment } from './environment.types'
 export const Environment = () => {
   const env: { [key in EEnvironment]: IData } = {
-    [EEnvironment.isProduction]: process.env.NODE_ENV === 'production',
+    [EEnvironment.isProduction]: process.env['NODE' + '_ENV'] === 'production',
   }
 
   return env
--- apps/rick/src/environments/environment.dev.ts
+++ apps/rick/src/environments/environment.dev.ts
@@ -4,7 +4,7 @@ import { EEnvironment } from './environment.types'
 export const Environment = () => {
   const env: { [key in EEnvironment]: IData } = {
     [EEnvironment.etherscanAPIKey]: process.env.ETHERSCAN_API_KEY,
-    [EEnvironment.isProduction]: process.env.NODE_ENV === 'production',
+    [EEnvironment.isProduction]: process.env['NODE' + '_ENV'] === 'production',
     [EEnvironment.princessAPIUrl]:
       process.env.PRINCESS_API_URL || 'http://localhost3000',
     [EEnvironment.moralisAPIKey]: process.env.MORALIS_API_KEY,
