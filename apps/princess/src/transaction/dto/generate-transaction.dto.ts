0e2c1611c37d04c643bcd9a11399016e83dc3ca9
polartar <polaris.center@outlook.com>
2023-07-25 10:35:00 -0700

    chore: update swagger response

--- apps/princess/src/transaction/dto/generate-transaction.dto.ts
+++ apps/princess/src/transaction/dto/generate-transaction.dto.ts
@@ -59,10 +59,10 @@ export class GenerateTransactionSwaggerResponse {
 
   @ApiProperty({
     example: {
-      signingPayloads:
-        '[{"address":"0xe456f9A32E5f11035ffBEa0e97D1aAFDA6e60F03","publickey":"0295deb418112ccbf9ff1d5c81cbdbd74503016d143f087ad2858facde962bebbc","tosign":"df098f069542d5c6973d55d8e1c247158d1f2bdd0284cb8755b3c275886bba0d"}]',
-      serializedTransaction:
-        '{"type":2,"from":"0xe456f9A32E5f11035ffBEa0e97D1aAFDA6e60F03","to":"0xe456f9A32E5f11035ffBEa0e97D1aAFDA6e60F03","value":{"value":"0.0001","factor":0},"extra":{"transferMessage":"merhaba","publicKey":"0295deb418112ccbf9ff1d5c81cbdbd74503016d143f087ad2858facde962bebbc","encodedDataMessage":"0x6d657268616261"},"fee":{"fee":{"value":"0.000039196713458448","factor":0},"extra":{"gas":21112,"gasPrice":"1856608254"}},"nativeTransaction":{"nonce":"0x26f","gasPrice":"0x6ea997fe","gasLimit":"0x5278","to":"0xe456f9a32e5f11035ffbea0e97d1aafda6e60f03","value":"0x5af3107a4000","data":"0x6d657268616261","chainId":5},"signingPayloads":[{"address":"0xe456f9A32E5f11035ffBEa0e97D1aAFDA6e60F03","publickey":"0295deb418112ccbf9ff1d5c81cbdbd74503016d143f087ad2858facde962bebbc","tosign":"df098f069542d5c6973d55d8e1c247158d1f2bdd0284cb8755b3c275886bba0d"}]}',
+      // eslint-disable-next-line no-useless-escape
+      signingPayloads: `"[{\"address\":\"0xe456f9A32E5f11035ffBEa0e97D1aAFDA6e60F03\",\"publickey\":\"0295deb418112ccbf9ff1d5c81cbdbd74503016d143f087ad2858facde962bebbc\",\"tosign\":\"df098f069542d5c6973d55d8e1c247158d1f2bdd0284cb8755b3c275886bba0d\"}]"`,
+      // eslint-disable-next-line no-useless-escape
+      serializedTransaction: `{\"type\":2,\"from\":\"0xe456f9A32E5f11035ffBEa0e97D1aAFDA6e60F03\",\"to\":\"0xe456f9A32E5f11035ffBEa0e97D1aAFDA6e60F03\",\"value\":{\"value\":\"0.0001\",\"factor\":0},\"extra\":{\"transferMessage\":\"merhaba\",\"publicKey\":\"0295deb418112ccbf9ff1d5c81cbdbd74503016d143f087ad2858facde962bebbc\",\"encodedDataMessage\":\"0x6d657268616261\"},\"fee\":{\"fee\":{\"value\":\"0.000039196713458448\",\"factor\":0},\"extra\":{\"gas\":21112,\"gasPrice\":\"1856608254\"}},\"nativeTransaction\":{\"nonce\":\"0x26f\",\"gasPrice\":\"0x6ea997fe\",\"gasLimit\":\"0x5278\",\"to\":\"0xe456f9a32e5f11035ffbea0e97d1aafda6e60f03\",\"value\":\"0x5af3107a4000\",\"data\":\"0x6d657268616261\",\"chainId\":5},\"signingPayloads\":[{\"address\":\"0xe456f9A32E5f11035ffBEa0e97D1aAFDA6e60F03\",\"publickey\":\"0295deb418112ccbf9ff1d5c81cbdbd74503016d143f087ad2858facde962bebbc\",\"tosign\":\"df098f069542d5c6973d55d8e1c247158d1f2bdd0284cb8755b3c275886bba0d\"}]}`,
       serverSignature:
         'NgsBb4xDEQpWkO3z74WayA46cDTnMVSTXr8Q8nIsdV8tNmA48RKLAOEo5RqP7FKrj1qOgelFWqvPABCcgR4ess00+DYoBJyMhBP0Ps8GyLKFsL6SVHkCcdPfW+hTadrClLD1ZNojjmbV4jzmJOwZs969Ft9BQOOG51u4byr9E4A=',
     },
