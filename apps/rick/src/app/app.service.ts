9673a65ea338b9667166e30d8dd997dfd5766917
polartar <polaris.center@outlook.com>
2023-08-16 06:04:45 -0700

    feat(rick): add env check function

--- apps/rick/src/app/app.service.ts
+++ apps/rick/src/app/app.service.ts
@@ -1,7 +1,25 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, InternalServerErrorException } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { EEnvironment } from '../environments/environment.types'
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {
+    this.checkEnvs()
+  }
+
+  checkEnvs() {
+    Object.keys(EEnvironment).map((envKey) => {
+      const value = this.configService.get<string | boolean>(
+        EEnvironment[envKey],
+      )
+      if (!value && value !== false) {
+        throw new InternalServerErrorException(
+          `Env variable(${envKey}) is missing`,
+        )
+      }
+    })
+  }
   getData(): { message: string } {
     return { message: 'Welcome to rick!' }
   }
