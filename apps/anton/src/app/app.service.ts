ab632e4eedc0473333cf7a61b56af141f62085b6
polartar <polaris.center@outlook.com>
2023-08-29 07:48:50 -0700

    chore: remove anton service

--- apps/anton/src/app/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common'
-
-@Injectable()
-export class AppService {
-  static welcomeMessage = 'Anton is up and running'
-  get welcomeMessage() {
-    return AppService.welcomeMessage
-  }
-}
