b509eaebfc07c28b50cd1cc7d19906933b2c9379
polartar <polaris.center@outlook.com>
2023-08-29 07:49:06 -0700

    chore: remove morty service

--- apps/morty/src/environments/environment.types.ts
+++
@@ -1,6 +0,0 @@
-export enum EEnvironment {
-  coinMarketAPI = 'coin_market_api',
-  fidelityClientId = 'fidelity_client_id',
-  fidelityClientSecret = 'fidelity_client_secret',
-  princessAPIUrl = 'princess_api_url',
-}
