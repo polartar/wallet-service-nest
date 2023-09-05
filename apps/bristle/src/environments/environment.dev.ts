74d1786e97b2a04417f9e5da08dd32ad68056103
polartar <polaris.center@outlook.com>
2023-09-05 09:14:12 -0700

    chore: remove bristle

--- apps/bristle/src/environments/environment.dev.ts
+++
@@ -1,8 +0,0 @@
-import { IData } from '@rana/core'
-
-import { EEnvironment } from './environment.types'
-export const Environment = () => {
-  const env: { [key in EEnvironment]: IData } = {}
-
-  return env
-}
