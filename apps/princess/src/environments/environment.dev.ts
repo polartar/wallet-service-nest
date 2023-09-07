d3498a4a9c1c33947e4aef91ee33bdfc05565136
polartar <polaris.center@outlook.com>
2023-09-07 07:03:56 -0700

    chore(princess): remove infura env variable

--- apps/princess/src/environments/environment.dev.ts
+++ apps/princess/src/environments/environment.dev.ts
@@ -5,7 +5,6 @@ export const Environment = () => {
   const env: { [key in EEnvironment]: IData } = {
     [EEnvironment.fidelityClientId]: process.env.FIDELITY_CLIENT_ID,
     [EEnvironment.fidelityClientSecret]: process.env.FIDELITY_CLIENT_SECRET,
-    [EEnvironment.infuraAPIKey]: process.env.INFURA_API_KEY,
     [EEnvironment.rickAPIUrl]:
       process.env.RICK_API_URL || 'http://localhost:3333',
     [EEnvironment.mortyAPIUrl]:
--- apps/princess/src/environments/environment.types.ts
+++ apps/princess/src/environments/environment.types.ts
@@ -1,7 +1,6 @@
 export enum EEnvironment {
   fidelityClientId = 'fidelity_client_id',
   fidelityClientSecret = 'fidelity_client_secret',
-  infuraAPIKey = 'infura_api_key',
   rickAPIUrl = 'rick_api_url',
   mortyAPIUrl = 'morty_api_url',
   gandalfAPIUrl = 'gandalf_api_url',
